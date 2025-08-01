f5b71a56c6e0ed235727eb160a13d73bda72c58e
Udo Nkwocha <udokah@meta.com>
2025-08-01 12:04:10 +0100

    allow own domain

--- backend/rag-pipeline/api.py
+++ backend/rag-pipeline/api.py
@@ -19,6 +19,9 @@ allowed_cors_origins = [
     "http://127.0.0.1:4000",
     "http://127.0.0.1:8080",
     "http://frontend:4000"
+    "http://frontend:3000",
+    "http://law.9jagpt.com",
+    "https://law.9jagpt.com"
 ]
 
 app = FastAPI(
